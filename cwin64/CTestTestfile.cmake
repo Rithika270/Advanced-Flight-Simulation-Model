b091a55289ee5978c9873f5d86855b406561e494
Rithika270 <157670419+Rithika270@users.noreply.github.com>
2024-07-27 01:58:38 -0400

    Add files via upload

---
+++ cwin64/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/repos/repo_distro/aburn/usr/modules/NewModule/src
+# Build directory: C:/repos/repo_distro/aburn/usr/modules/NewModule/cwin64
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
